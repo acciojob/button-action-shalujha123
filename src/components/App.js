397bbd4fa42e4aac40177ffca3464ddacc3a48dc
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-24 20:21:49 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = (props) => {
+  const [state, setState] = useState(false);
+
+  function handleToggle() {
+    setState(true);
+  }
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className={state ? "show" : "hide"}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+      <button id="click" onClick={handleToggle}>
+        Show
+      </button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
